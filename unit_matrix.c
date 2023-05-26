e87b9668d0d6056ca7f30f97ae4e4a40b30ee885
Sabnam Parvin <114932621+sabnamsp@users.noreply.github.com>
2023-05-26 11:17:59 +0600

    Update unit_matrix.c

--- unit_matrix.c
+++ unit_matrix.c
@@ -1,1 +1,40 @@
+#include <stdio.h>
+#include <stdbool.h>
 
+int main() 
+{
+  int n;
+  scanf("%d", &n);
+
+  int matrix[n][n];
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      scanf("%d", &matrix[i][j]);
+    }
+  }
+  bool isUnitMatrix = true;
+  for (int i = 0; i < n; i++) 
+  {
+    for (int j = 0; j < n; j++) 
+    {
+      if (i == j && matrix[i][j] != 1) 
+      {
+        isUnitMatrix = false;
+        break;
+      } else if (i != j && matrix[i][j] != 0) 
+      {
+        isUnitMatrix = false;
+        break;
+      }
+    }
+  }
+
+  if (isUnitMatrix)
+  {
+    printf("YES\n");
+  } else
+  {
+    printf("NO\n");
+  }
+  return 0;
+}
